f42820247e8f7b247208f8935d891ad10bc7586e
Justinas <andriekus.justinas@gmail.com>
2019-05-28 15:45:57 +0300

    Removed backend folder

--- backend/Reservation/Reservations/ViewModels/WorkTimeViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Reservations.ViewModels
-{
-    public partial class WorkTimeViewModel
-    {
-        public int Id { get; set; }
-        public int MinutesFrom { get; set; }
-        public int MinutesTo { get; set; }
-    }
-}
